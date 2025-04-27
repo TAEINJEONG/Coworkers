3f7cbbcf9a7b4f202b9ef97454696c22a16555dc
TAEINJEONG <taeinjeong46@gmail.com>
2025-04-27 14:24:08 +0900

    fix/next.config.js __dirname 참조하는 부분 제거

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(process.cwd(), 'src'),
+    };
     return config;
   },
 };
